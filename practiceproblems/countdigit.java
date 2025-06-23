d73cba680b892ead022ac6e6245c8bb02438656e
POORNIMA-4196@gmail.com <POORNIMA-4196@gmail.com>
2025-06-23 17:00:35 +0530

    Initial commit: Added LeetCode, GFG, and practiced problems

---
+++ practiceproblems/countdigit.java
@@ -0,0 +1,27 @@
+package practiceproblems;
+
+public class countdigit {
+  
+static int count(int n)
+{
+  int   cnt=0;
+  while(n>0)
+  {
+    cnt=cnt+1;
+    n=n/10;
+
+  }
+  return cnt;
+
+}
+    public static void main(String args[])
+    {
+        int n=1000;
+        System.out.println("n:"+n);
+        int digits=count(n);
+        System.out.println(digits);
+
+
+    }
+}
+
