e18784b8493ee6de5d08d8c02758950bd741a988
POORNIMA-4196@gmail.com <POORNIMA-4196@gmail.com>
2025-06-23 21:04:26 +0530

    Fibonacci number -509

---
+++ Leetcode-DSA/ID509.java
@@ -0,0 +1,23 @@
+/*  ✅ Problem Title: Fibonacci Number
+    🔗 LeetCode Link: https://leetcode.com/problems/fibonacci-number/
+    📄 One-line Description: Return the nth Fibonacci number using recursion (0-based indexing).
+    
+    💡 Approach: Simple recursion
+    ⏰ Time Complexity: O(2^n)
+    🧠 Space Complexity: O(n) — Recursion stack space */
+
+public class ID509{
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int last = fib(n - 1);      
+        int slast = fib(n - 2);    
+        return last + slast;        
+    }
+    public static void main(String args[])
+    {
+        int n=6;
+        System.out.println(fib( n));
+    }
+} 
